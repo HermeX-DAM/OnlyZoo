3443d8d9bb98c8fc33fd62947b4883055647336b
tarik <salahitarik@gmail.com>
2023-11-29 21:36:27 +0100

    Metodo Mostrar mascotas#7

---
+++ OnlyZoo_Project/OnlyZoo_Project/Conexion/ConexionBBDD.cs
@@ -0,0 +1,52 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlyZoo_Project.Conexion
+{
+    internal class ConexionBBDD
+    {
+        public readonly string cadenaConexion;
+        public MySqlConnection conexion;
+
+        public ConexionBBDD(string servidor, string baseDatos, string usuario, string contraseña)
+        {
+            cadenaConexion = $"Server={servidor};Database={baseDatos};User ID={usuario};Password={contraseña};";
+        }
+
+        public void AbrirConexion()
+        {
+            try
+            {
+                conexion = new MySqlConnection(cadenaConexion);
+                conexion.Open();
+                Console.WriteLine("Conexión abierta");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al abrir la conexión: {ex.Message}");
+            }
+        }
+
+        public void CerrarConexion()
+        {
+            try
+            {
+                if (conexion.State == ConnectionState.Open)
+                {
+                    conexion.Close();
+                    Console.WriteLine("Conexión cerrada");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al cerrar la conexión: {ex.Message}");
+            }
+        }
+
+    }
+}
---
+++ OnlyZoo_Project/OnlyZoo_Project/Controlador/Controlador.cs
@@ -0,0 +1,60 @@
+﻿using MySql.Data.MySqlClient;
+using OnlyZoo_Project.Conexion;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OnlyZoo_Project.Controlador
+{
+    internal class Controlador
+    {
+
+
+    }
+
+
+    public List<Mascota> MostrarMascotas()
+    {
+        List<Mascota> mascotas = new List<Mascota>();
+        ConexionBBDD conexionBBDD = new ConexionBBDD("localhost", "tuBaseDeDatos", "tuUsuario", "tuContraseña");
+        // Abre la conexión
+        conexionBBDD.AbrirConexion();
+        string query = "SELECT * FROM Mascotas";
+
+        using (MySqlCommand command = new MySqlCommand(query, conexionBBDD.conexion))
+        using (MySqlDataReader reader = command.ExecuteReader())
+        {
+            while (reader.Read())
+            {
+                Mascota mascota = new Mascota()
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Nombre = Convert.ToString(reader["Nombre"]),
+                    Raza = Convert.ToString(reader["Raza"]),
+                    Edad = Convert.ToInt32(reader["Edad"]),
+                    Dueño = Convert.ToString(reader["Dueño"]),
+                    Vacunada = Convert.ToBoolean(reader["Vacunada"]),
+                    Color = Convert.ToString(reader["Color"]),
+                };
+                reader.Close();
+                mascotas.Add(mascota);
+            }
+        }
+        conexionBBDD.CerrarConexion();
+        return mascotas;
+    }
+
+      
+
+
+
+}
+
+    
+
+            
+
